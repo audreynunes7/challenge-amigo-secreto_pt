b93e863b982da0e81b9d1808dee5fff44459b140
Audrey Nunes <audrey_nunes@outlook.com>
2025-03-16 17:35:59 -0300

    Aplicando funcionalidades do site

---
+++ README.md
@@ -0,0 +1,6 @@
+## Descrição
+Projeto elaborado para o Challenge Amigo Secreto da Alura
+
+1 - Escreva o seu nome e dos seus amigos que irão participar
+
+2 - Clique em Sortear amigo
--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let lista = [];
+
+function adicionarAmigo() {
+
+    let input = document.querySelector('input');
+    let valor = input.value.trim();
+
+    if(valor == '') {
+        alert('Informe um nome válido');
+        return;
+    }
+
+    lista.push(valor);
+    input.value = '';
+    input.focus();
+
+    renderizarLista();
+}
+
+function sortearAmigo() {
+    if (lista.length <= 0){
+        alert ('A lista está vazia.');
+        return;
+    } 
+    let sorteio = Math.floor(Math.random() * lista.length);
+    document.querySelector('#resultado').innerHTML = `O(a) amigo(a) secreto(a) é: ${lista[sorteio]}`;
+    lista = []
+    renderizarLista();
+}
+
+function renderizarLista() {
+
+    var ul = document.querySelector('#listaAmigos');
+    ul.innerHTML = '';
+
+    lista.forEach(function(i){
+        var li = document.createElement('li');
+        li.innerHTML = i;
+        ul.append(li);
+    });
+}
